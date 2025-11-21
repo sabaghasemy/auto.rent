58f07c07c5182f8930477b89bcc41a65c27445e7
Hasti <hastiitaherii2006@gmail.comgit clone [200~https://github.com/sabaghasemy/auto.rent.git~git config --global user.email hastiitaherii2006@gmail.com>
2025-11-21 13:07:06 +0330

    css

---
+++ main.js
@@ -0,0 +1,6 @@
+fetch('db.json') 
+  .then(response => response.json()) // تبدیل به شیء JS
+  .then(data => {
+    console.log(data); // استفاده از داده‌ها
+  })
+  .catch(error => console.error('Error:', error));
